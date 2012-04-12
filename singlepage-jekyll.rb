7ba55ddf14a8d37e46fb7b00337f51bfcb868c71
Jacob Klapwijk <hello@jcbk.me>
2012-04-12 19:19:23 +0100

    oops, bug squashed.

--- singlepage-jekyll.rb
+++ singlepage-jekyll.rb
@@ -3,8 +3,8 @@
 require 'rubygems'
 require 'json'
 
-files = Dir.chdir('.') do
-  Dir.glob('*.html').map{ |f| f.chomp('.html') }
+files = Dir.chdir('_site') do
+  Dir.glob('**/*.html').map{ |f| f.chomp('.html') }
 end
 
 File.open("html.json","w") do |f|
